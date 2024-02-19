e3f5b7874c403aad93c44af877f365488aa0b2eb
Jormmm <146562442+Jormmm@users.noreply.github.com>
2024-02-19 20:07:46 +0200

    Add files via upload

--- build/js/scripts.js
+++ build/js/scripts.js
@@ -84,3 +84,94 @@ function tabs(
 // ЧЕТВЕРТЫЙ аргумент - класс активности, который будет добавлятся для таба, который сейчас активен.
 tabs('.tabs__header', '.tabs__picture', '.tabs__content-item', 'activate')
 
+const phoneInput = document.querySelector('.form-number')
+// const btn = document.querySelector('.form__button');
+
+const mask = new IMask(phoneInput, {
+    mask: "+{38}(000)0000000",
+});
+
+const phoneInput2 = document.querySelector('.connection-form-number')
+// const btn = document.querySelector('.form__button');
+
+const mask2 = new IMask(phoneInput2, {
+    mask: "+{38}(000)0000000",
+});
+
+// в эту константу помещаем URL развёрнутого веб-приложения Google Apps Script
+// ВНИМАНИЕ! Это должен быть адрес ВАШЕГО РАЗВЕРНУТОГО ПРИЛОЖЕНИЯ
+// ТЕКУЩИЙ URL_APP приведён для примера
+const URL_APP =
+	'https://script.google.com/macros/s/AKfycbxVdz38mxZPoZikGur742JoPM8wGmXgMow1hVzn6jRfmRrxN_R1yrdHb2DhLRWSy3oHLQ/exec'
+
+// находим форму в документе
+const form = document.querySelector('#hero-form')
+
+// указываем адрес отправки формы (нужно только в начале примера)
+form.action = URL_APP
+
+// вспомогательная функция проверки заполненности формы
+function isFilled(details) {
+	const { name, email, phone} = details
+	if (!name) return false
+	if (!email) return false
+	if (!phone) return false
+	return true
+}
+
+// навешиваем обработчик на отправку формы
+form.addEventListener('submit', async ev => {
+	// отменяем действие по умолчанию
+	ev.preventDefault()
+
+	// получаем ссылки на элементы формы
+	const name = document.querySelector('[name=name]')
+	const email = document.querySelector('[name=email]')
+	const phone = document.querySelector('[name=phone]')
+
+	// собираем данные из элементов формы
+	let details = {
+		name: name.value.trim(),
+		email: email.value.trim(),
+		phone: phone.value.trim()
+	}
+
+	// если поля не заполнены - прекращаем обработку
+	if (!isFilled(details)) return
+
+	// подготавливаем данные для отправки
+	let formBody = []
+	for (let property in details) {
+		// кодируем названия и значения параметров
+		let encodedKey = encodeURIComponent(property)
+		let encodedValue = encodeURIComponent(details[property])
+		formBody.push(encodedKey + '=' + encodedValue)
+	}
+	// склеиваем параметры в одну строку
+	formBody = formBody.join('&')
+
+	// выполняем отправку данных в Google Apps
+	const result = await fetch(URL_APP, {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
+		},
+		//cors: "no-cors", <- это неправильно
+		mode: 'cors', //<- оставим по умолчанию
+		body: formBody,
+	})
+		.then(res => res.json())
+		.catch(err => alert('Ошибка!'))
+	// .then((res) => console.log(res));
+
+	if (result.type === 'success') {
+		name.value = ''
+		email.value = ''
+		phone.value = ''
+		alert('Дякуємо!')
+	}
+	if (result.type === 'error') {
+		alert(`Ошибка( ${result.errors}`)
+	}
+})
+
